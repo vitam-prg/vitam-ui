39cd2bc206ee6d07e856d8154999f5f6d54ea634
Baptiste Toulemonde <toulemonde@cines.fr>
2021-04-14 19:15:11 +0200

    fix: api url

--- ui/ui-frontend/projects/pastis/src/environments/environment.ts
+++ ui/ui-frontend/projects/pastis/src/environments/environment.ts
@@ -43,7 +43,7 @@ import{IEnvironment} from './IEnvironment'
 
 export const environment: IEnvironment= {
   production: false,
-  apiServerUrl: "https://dev.vitamui.com:8051"
+  apiServerUrl: "https://sandbox78-adm.cines.fr:8051"
 };
 
 /*
