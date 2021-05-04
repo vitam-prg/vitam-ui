a39e5cc91ed4d7d576d270e19099313c89421901
bbenarbia <benaissa.benarbia@gmail.com>
2021-05-04 17:40:03 +0200

    fix wrong ingest error

--- ui/ui-ingest/src/main/java/fr/gouv/vitamui/ingest/thread/IngestThread.java
+++ ui/ui-ingest/src/main/java/fr/gouv/vitamui/ingest/thread/IngestThread.java
@@ -1,5 +1,6 @@
 package fr.gouv.vitamui.ingest.thread;
 
+import fr.gouv.vitamui.commons.api.CommonConstants;
 import fr.gouv.vitamui.commons.api.logger.VitamUILogger;
 import fr.gouv.vitamui.commons.api.logger.VitamUILoggerFactory;
 import fr.gouv.vitamui.commons.rest.client.ExternalHttpContext;
@@ -7,7 +8,10 @@ import fr.gouv.vitamui.ingest.external.client.IngestExternalWebClient;
 import fr.gouv.vitamui.ingest.service.IngestService;
 import org.springframework.web.reactive.function.client.ClientResponse;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * Thread that send the uploaded stream to vitamui ingest-external through it's client
@@ -45,15 +49,32 @@ public class IngestThread extends Thread {
             if (!response.statusCode().is2xxSuccessful()) {
                 LOGGER.debug("Upload of [{}] failed. StatusCode : [{}] .", originalFilename,
                     response.statusCode());
+                deleteTempFiles();
             }
 
             if (response.statusCode().is2xxSuccessful()) {
                 LOGGER.debug("Upload of [{}] succeeded with StatusCode : [{}].", originalFilename,
                     response.statusCode());
+                deleteTempFiles();
             }
-
         } catch (final Exception e) {
             LOGGER.debug("ERROR : Upload of [{}] failed.\n [{}]", originalFilename, e.getMessage());
+        }
+    }
+
+    private void deleteTempFiles() {
+        try {
+            LOGGER.info("Try to delete temp file {} ", originalFilename);
+            Files.deleteIfExists(
+                Paths.get(System.getProperty(CommonConstants.VITAMUI_TEMP_DIRECTORY), context.getRequestId()));
+            Files.deleteIfExists(
+                Paths.get(System.getProperty(CommonConstants.VITAMUI_TEMP_DIRECTORY),
+                    "int-" + context.getRequestId()));
+            Files.deleteIfExists(
+                Paths.get(System.getProperty(CommonConstants.VITAMUI_TEMP_DIRECTORY),
+                    "ext-" + context.getRequestId()));
+        } catch (IOException e) {
+            LOGGER.error("Error deleting temp file {} error {} ", originalFilename, e.getMessage());
         }
     }
 
